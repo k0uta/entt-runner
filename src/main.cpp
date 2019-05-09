78ae9b5d569fe38f5f78bbaa54adff9984b0c114
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2019-05-09 08:44:00 -0300

    refactor(app): improve app instantiation / deletion

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include "core/app.hpp"
 
 int main() {
+    auto app = new App();
     try {
-        auto app = new App();
         app->run();
     } catch (std::exception &e) {
         std::cout << e.what() << '\n';
         return 1;
     }
+    delete(app);
+
     return 0;
 }
