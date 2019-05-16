b4efc6dde00b8591e5e1858094ed14919ee17d76
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2019-05-16 08:30:02 -0300

    feat(physics): add body & speed components

---
+++ src/components/physics.hpp
@@ -0,0 +1,13 @@
+#ifndef ENTT_RUNNER_PHYSICS_HPP
+#define ENTT_RUNNER_PHYSICS_HPP
+
+struct body {
+    float mass;
+};
+
+struct speed {
+    float x;
+    float y;
+};
+
+#endif //ENTT_RUNNER_PHYSICS_HPP
